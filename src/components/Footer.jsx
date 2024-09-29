8ee13bd91d43f7a1c9a233f32bd6266385d5c357
Khawm Mung <johnmungtawng5@gmail.com>
2024-09-28 19:52:05 -0500

    adding links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,10 @@ function Footer() {
             <p>©2024 
                 <Link to='/'> Zomi YD</Link>
             </p>
+           <p>
+                Made with ❤️ by
+                <a href='https://kmung.github.io/' target='_blank' rel='noreferrer nofollow'> KM</a>
+           </p>
         </footer>
     );
 }
